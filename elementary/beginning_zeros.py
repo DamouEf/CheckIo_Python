76a7af30803d05a0eb2709f1b22557c04046b27f
Wael Fridhi <Wael.Fridhi@tunix.io>
2020-08-24 22:31:14 +0100

    [elementary] beginning zeros

---
+++ elementary/beginning_zeros.py
@@ -0,0 +1,23 @@
+'''
+You have a string that consist only of digits. You need to find how many zero digits ("0") are at the beginning of the given string.
+Input: A string, that consist of digits.
+Output: An Int.
+'''
+
+
+def beginning_zeros(number: str) -> int:
+    return len(number) if int(number) == 0 else (len(number)) - len(str(int(number)))
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(beginning_zeros('100'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert beginning_zeros('100') == 0
+    assert beginning_zeros('001') == 2
+    assert beginning_zeros('100100') == 0
+    assert beginning_zeros('001001') == 2
+    assert beginning_zeros('012345679') == 1
+    assert beginning_zeros('0000') == 4
+    print("Coding complete? Click 'Check' to earn cool rewards!")
